d2a9078e4a4470a861ab2ea3c1441b0f150e4472
Victoriast8 <42806739+Victoriast8@users.noreply.github.com>
2020-01-24 10:04:51 +0100

    Database + server
    Updated clienthandler, sqlhandler and server

--- Server/src/handling/ClientHandler.java
+++ Server/src/handling/ClientHandler.java
@@ -1,9 +1,36 @@
 package handling;
 
-public class ClientHandler {
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.Socket;
+
+import server.Server;
+
+public class ClientHandler implements Runnable, Closeable {
 	
-	public ClientHandler() {
+	Socket socket;
+	Server server;
+	
+	public ClientHandler(Server server, Socket socket, int clientHandlerNumber) {
+		this.server = server;
+		this.socket = socket;
 		
+		new Thread("Client handler " + clientHandlerNumber).start();
 	}
 	
+	public Socket getSocket() {
+		return socket;
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,30 +1,70 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.Closeable;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 import handling.ClientHandler;
 
-public class Server {
+public class Server implements Runnable, Closeable{
+	
+	private boolean stop;
 	
 	private ServerSocket serverSocket;
+	private Socket socket;
 	
 	private List<ClientHandler> clients;
 	
 	public Server() throws Exception {
-		serverSocket = new ServerSocket( 8777 );
-		Socket socket = serverSocket.accept();
+		serverSocket = new ServerSocket(8777);
+		socket = serverSocket.accept();
 		
-		PrintStream ps = new PrintStream( socket.getOutputStream(), false, Charset.forName("UTF-8") );
+		clients = new ArrayList<>();
+		
+		new Thread("ServerThread").start();
+		
+		/*PrintStream ps = new PrintStream(socket.getOutputStream(), false, Charset.forName("UTF-8"));
 		ps.println("det virker");
 		
-		BufferedReader br = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
-		System.out.println(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		System.out.println(br.readLine());*/
+	}
+
+	@Override
+	public void run() {
+		try {
+			clients.add(new ClientHandler(this, serverSocket.accept(), clients.size()));
+			System.out.println("added new client to client list");
+		}catch (IOException e) {
+			if(stop == true) {
+				return;
+			}
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public boolean getStop() {
+		return stop;
+	}
+	
+	public void requestRemoval(ClientHandler c) {
+		clients.remove(c);
+	}
+
+	@Override
+	public void close() throws IOException {
+		stop = true;
+		
+		serverSocket.close();
+		
+		for(ClientHandler c : clients) {
+			c.close();
+		}
+		
 	}
 	
 }
